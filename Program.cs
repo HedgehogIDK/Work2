c85afd73638f9d1a30cfb6a30f30ed81b90bf955
Hegehog <workjob2077@gmail.com>
2025-04-18 23:24:13 +0300

    Задание 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using static System.Console;
 
-Exercise1();
+//Exercise1();
+Exercise2();
 
 void Exercise1()
 {
@@ -73,4 +74,65 @@ void Exercise1()
     WriteLine($"Сумма чётных элементов массива A: {sumEven}");
     WriteLine($"Сумма элементов нечётных столбцов массива B: {sumOddColumns}");
 
+}
+
+void Exercise2()
+{
+    int[][] mas = new int[5][];
+    Random rand = new Random();
+    int min = int.MaxValue, max = int.MinValue;
+    int minRow = 0, minCol = 0, maxRow = 0, maxCol = 0;
+    int value, sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        mas[i] = new int[5];
+    }
+
+    Console.WriteLine("Массив:");
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            value = rand.Next(-100, 101);
+            mas[i][j] = value;
+
+            Console.Write($"{value,5}");
+
+            if (value < min)
+            {
+                min = value;
+                minRow = i;
+                minCol = j;
+            }
+            if (value > max)
+            {
+                max = value;
+                maxRow = i;
+                maxCol = j;
+            }
+        }
+        Console.WriteLine();
+    }
+
+    int minPos = minRow * 5 + minCol;
+    int maxPos = maxRow * 5 + maxCol;
+
+    if (minPos > maxPos)
+    {
+        int temp = minPos;
+        minPos = maxPos;
+        maxPos = temp;
+    }
+
+    for (int i = minPos + 1; i < maxPos; i++)
+    {
+        int row = i / 5;
+        int col = i % 5;
+        sum += mas[row][col];
+    }
+
+    Console.WriteLine($"\nМинимум: {min}, максимум: {max}");
+    Console.WriteLine($"Сумма между ними: {sum}");
 }
